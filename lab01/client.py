7848a0640e204ae224ff82429c9ec4e606f9a2d7
SebastianRychert <sebastian_rychert@wp.pl>
2022-10-23 12:15:50 +0200

    lab01

---
+++ lab01/client.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+data = 'dane.txt'
+results = 'wyniki.txt'
+
+time_stamp = 0.0
+
+if (os.path.exists(results)):
+    time_stamp = os.stat(results).st_mtime
+
+liczba = input('Podaj liczbe całkowitą: ')
+try:
+    int(liczba)
+except ValueError:
+    print(liczba, 'nie jest liczbą całkowitą')
+    exit()
+
+with open(data, 'w') as file:
+    file.write(liczba)
+
+
+while (True):
+    if not (os.path.exists(results)):
+        time.sleep(0.1)
+        continue
+
+    if (os.stat(results).st_mtime != time_stamp):
+        with open(results, 'r') as f:
+            last_line = f.readlines()[-1]
+            print(last_line)
+            break
---
+++ lab01/server.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+data = 'dane.txt'
+results = 'wyniki.txt'
+
+
+def prosty_wielomian(x):
+    return 5 * x**2 + 2*x + 1
+
+
+def save_to_results(data):
+    with open(results, "a") as file:
+        file.write(str(data) + "\n")
+
+
+time_stamp = 0.0
+if not (os.path.exists(data)):
+    with open(data, 'w') as file:
+        file.write('')
+        time_stamp = os.stat(data).st_mtime
+
+while (True):
+    new_time_stamp = os.stat(data).st_mtime
+    if (new_time_stamp != time_stamp):
+        with open(data, 'r') as file:
+            liczba = int(file.read())
+            result = prosty_wielomian(liczba)
+            save_to_results(result)
+            time_stamp = new_time_stamp
+    else:
+        time.sleep(1)
