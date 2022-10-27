417a870cabd53dc323a62fcfdcb6c5d15badc14c
SebastianRychert <sebastian_rychert@wp.pl>
2022-10-27 13:29:53 +0200

    zad3 files

---
+++ lab03/countWords.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+
+
+import sys
+# sys.setdefaultencoding() does not exist, here!
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+parser = argparse.ArgumentParser(
+    description='Fork searching')
+parser.add_argument('--p', type=str, help='File path to begin search')
+parser.add_argument('--s', type=str, help='Word to search for')
+
+args = parser.parse_args()
+
+if (args.p is None):
+    exit("'--p' flag not found")
+
+if (args.s is None):
+    exit("'--s' flag not found")
+
+# rozgałęziamy proces
+pid = os.fork()
+
+print(pid)
