ff92a6c77e4aab271c6e0ed095e52d08e8334d1b
SebastianRychert <sebastian_rychert@wp.pl>
2022-10-31 16:44:01 +0100

    zliczanie liczby wystapien slowa w plikach

--- lab03/countWords.py
+++ lab03/countWords.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
-import sys
 
 parser = argparse.ArgumentParser(
     description='Fork searching')
@@ -16,7 +15,50 @@ if (args.p is None):
 if (args.s is None):
     exit("'--s' flag not found")
 
-# rozgałęziamy proces
-pid = os.fork()
 
-print(pid)
+def readFile(path):
+    with open(path, "r") as file:
+        lines = file.readlines()
+        files = []
+        count = 0
+
+        for line in lines:
+            if "\input" in line:
+                start = line.find("{")+1
+                end = line.find("}")
+
+                files.append(line[start:end])
+            else:
+                count += line.count(args.s)
+
+    return {"files": files, "count": count}
+
+
+def work(files, count):
+    fileToRead = files.pop()
+    result = readFile(fileToRead)
+    print("Znalazłem %d wystapienie słowa %s w pliku %s" %
+          (result["count"], args.s, fileToRead))
+
+    files += result["files"]
+    count += result["count"]
+
+    if len(files) == 0:
+        return count
+
+    pid = os.fork()
+
+    # proces macierzysty
+    if pid > 0:
+        # czekamy na wynik od jakiegoś syna
+        status = os.wait()
+        if os.WIFEXITED(status[1]):
+            return os.WEXITSTATUS(status[1])
+    # syn
+    else:
+        count2 = work(files, count)
+        os._exit(count2)
+
+
+word_count = work([args.p], 0)
+print(word_count)
