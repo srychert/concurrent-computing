5da08e1767ce14d2209f48c0627f1c282fef2433
SebastianRychert <sebastian_rychert@wp.pl>
2022-11-03 13:29:57 +0100

    obsulga sygnalow SIGHUP SIGTERM SIGUSR1

--- lab05/server.py
+++ lab05/server.py
@@ -1,5 +1,28 @@
 import sysv_ipc
 import time
+import signal
+import sys
+import os
+
+print(os.getpid())
+
+
+def handler(signum, frame):
+    print('Obsługa sygnału ', signum)
+
+
+def handler1(signum, frame):
+    print(' Inna obsługa sygnału ', signum)
+    sys.exit(0)
+
+
+# przypisanie obsługi sygnału do SIGINT
+signal.signal(signal.SIGHUP, handler)
+signal.signal(signal.SIGTERM, handler)
+
+
+# przypisanie obsługi sygnału do SIGUSR1
+signal.signal(signal.SIGUSR1, handler1)
 
 slownik = {
     "pies": "dog",
@@ -12,14 +35,12 @@ klucz2 = 12
 mq = sysv_ipc.MessageQueue(klucz, sysv_ipc.IPC_CREAT)
 mq2 = sysv_ipc.MessageQueue(klucz2, sysv_ipc.IPC_CREAT)
 
-
 # mq.remove()
 # mq2.remove()
 
 print("kolejka utworzona")
 
 while True:
-    time.sleep(20)
     s, t = mq.receive(True, 0)
     s = s.decode()
     print("Serwer: odebrałem %s  " % s)
@@ -30,6 +51,6 @@ while True:
         response = slownik[s]
 
     mq2.send(response.encode(), True)
-
-
-# mq.remove()
+    print("wysłane")
+    time.sleep(10)
+    print("po")
