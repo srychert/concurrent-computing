f44d78ec8c9ef3d0a32ad1228d1a4ab64d9dd681
SebastianRychert <sebastian_rychert@wp.pl>
2022-11-10 12:09:38 +0100

    lab04

--- lab04/server.py
+++ lab04/server.py
@@ -5,7 +5,8 @@ import time
 
 db = {
     0: "Kowalski",
-    1: "Nowak"
+    1: "Nowak",
+    999: "Malinowski"
 }
 
 FIFO = 'kolejka'
@@ -13,20 +14,20 @@ FIFO = 'kolejka'
 # utworzenie kolejki
 try:
     os.mkfifo(FIFO)
-except OSError as oe: 
+except OSError as oe:
     if oe.errno != errno.EEXIST:
         raise
 
-# kolejka otwarta do odczytu 
+# kolejka otwarta do odczytu
 fifo_in = os.open(FIFO, os.O_RDONLY)
 
 # kolejka otwarta do zapisu,
-# Żeby zakończenie klienta jej nie zamykało 
-fifo_out1 = os.open(FIFO, os.O_WRONLY|os.O_NDELAY) 
+# Żeby zakończenie klienta jej nie zamykało
+fifo_out1 = os.open(FIFO, os.O_WRONLY | os.O_NDELAY)
 
 while True:
     r = os.read(fifo_in, 27)
-    if len(r)>0:
+    if len(r) > 0:
         req = r.decode()
         req_list = req.split(" ")
         fifo_out2 = os.open(req_list[1], os.O_WRONLY)
@@ -34,7 +35,7 @@ while True:
         if int(req_list[0]) in db:
             response = db[int(req_list[0])]
         os.write(fifo_out2, response.encode())
-    else:       
-      print("Klient skończył")
-      break
-    time.sleep(1)
+    else:
+        print("Klient skończył")
+        break
+    time.sleep(5)
