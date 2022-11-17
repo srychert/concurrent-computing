8c15353ca44087fccc6dd6eb89947a3adda95171
SebastianRychert <sebastian_rychert@wp.pl>
2022-11-17 13:02:12 +0100

    board and turns

--- lab06/tic-tac-toe.py
+++ lab06/tic-tac-toe.py
@@ -1,22 +1,43 @@
 import sysv_ipc
+import time
+
+
 klucz = 31
 NULL_CHAR = '\0'
 
 
-def create(klucz: int):
-    sem1 = sysv_ipc.Semaphore(klucz, sysv_ipc.IPC_CREX, 0o700, 0)
-    sem2 = sysv_ipc.Semaphore(klucz+1, sysv_ipc.IPC_CREX, 0o700, 1)
-    mem = sysv_ipc.SharedMemory(klucz, sysv_ipc.IPC_CREX)
-    return sem1, sem2, mem
+def create_sem(key, init_value):
+    try:
+        sem = sysv_ipc.Semaphore(key)
+        return sem
+    except sysv_ipc.ExistentialError as e:
+        if (str(e) == "No semaphore exists with the specified key"):
+            return sysv_ipc.Semaphore(key, sysv_ipc.IPC_CREX, 0o700, init_value)
+        raise
 
 
-def remove(mem_id, sem1_id, sem2_id):
-    sysv_ipc.remove_shared_memory(mem_id)
-    sysv_ipc.remove_semaphore(sem1_id)
-    sysv_ipc.remove_semaphore(sem2_id)
+def remove_sem(sem):
+    sem.remove()
 
 
-sem1, sem2, mem = create(klucz)
+def create_mem(key):
+    try:
+        mem = sysv_ipc.SharedMemory(key)
+        return mem
+    except sysv_ipc.ExistentialError as e:
+        if (str(e) == f"No shared memory exists with the key {key}"):
+            return sysv_ipc.SharedMemory(klucz, sysv_ipc.IPC_CREX)
+        raise
+
+
+def remove_mem(mem):
+    mem.remove()
+
+
+sem = create_sem(klucz, 1)
+mem = create_mem(klucz)
+# sem.remove()
+# mem.remove()
 
 
 def pisz(mem, s):
@@ -34,16 +55,25 @@ def czytaj(mem):
     return s
 
 
-s = "test"
-pisz(mem, s)
-for i in range(0, 1):
-    print(s)
-    sem1.acquire(timeout=5)
-    s = czytaj(mem)
-    s = s+'a'
-    pisz(mem, s)
-    sem2.release()
+def next_player(player):
+    text = " to play\n"
+    next_player = ""
+    if player == "X" or player == " ":
+        next_player = "O"
+    elif player == "O":
+        next_player = "X"
+    else:
+        raise Exception(f"Wrong player type: {player}")
+
+    return next_player + text
 
 
-print(s)
-remove(mem.id, sem1.id, sem2.id)
+# Game here
+board = "_|_|_\n"*3
+sem.acquire()
+# time.sleep(3)
+odp = czytaj(mem)
+next = next_player(odp[0])
+pisz(mem, next + board)
+print(czytaj(mem))
+sem.release()
