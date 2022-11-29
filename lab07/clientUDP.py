648b1ec7a2eda4b06df1961ff99308a688a51bf1
SebastianRychert <sebastian_rychert@wp.pl>
2022-11-29 20:11:15 +0100

    rock paper scissors with UDP socket

--- lab07/clientUDP.py
+++ lab07/clientUDP.py
@@ -1,22 +1,37 @@
-# klient UDP, dwukrotmie  wysyła napis i odbiera napis
-
+import struct
 import socket
 
-komA = "aaaaa"
-komAB = str.encode(komA)
 serwerAdresPort = ("127.0.0.1", 5001)
-klientAdresPort = ("127.0.0.1", 5002)
 bufSize = 1024
-# tworzy gniazdo UDP po stronie klienta
 UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+score = 0
 
-# związuje gniazdo z parą adres, port - można pominąć
-# UDPClientSocket.bind(klientAdresPort)
+allowed_moves = ["R", "P", "S", "koniec"]
 
-# wysyła do serwera przez utworzone gniazdo
-UDPClientSocket.sendto(komAB, serwerAdresPort)
-odp = UDPClientSocket.recvfrom(bufSize)
-print(odp)
-UDPClientSocket.sendto("bbb".encode(), serwerAdresPort)
-odp = UDPClientSocket.recvfrom(bufSize)
-print(odp)
+while True:
+    move = input("\nPlay [R,P,S,koniec]: ")
+    if move not in allowed_moves:
+        print("invalid move")
+        continue
+
+    UDPClientSocket.sendto(str.encode(move), serwerAdresPort)
+
+    if move == "koniec":
+        break
+
+    print("Waiting for second player move")
+    answer = UDPClientSocket.recvfrom(bufSize)
+    answer = struct.unpack('!f', answer[0])[0]
+
+    if answer == 403:
+        print("2 players already playing")
+        break
+
+    if answer == -1:
+        print("Second player ended the game")
+        print(f"Final score: {score} points")
+        break
+
+    print(f"You got {answer} points")
+    score += answer
+    print(f"You have {score} points in total")
--- lab07/serverUDP.py
+++ lab07/serverUDP.py
@@ -1,5 +1,6 @@
-# serwer UDP, odbiera napis, odsyła ten napis na wielkich literach
 import socket
+import struct
+import pprint
 
 IP = "127.0.0.1"
 port = 5001
@@ -13,12 +14,61 @@ UDPServerSocket.bind((IP, port))
 
 print("serwer UDP działa")
 
-# obsługa nadchodzących datagramów
+
+def rock_paper_scissors(moves):
+    p1, p2 = moves
+
+    results = {
+        "PP": (0.5, 0.5), "PR": (1, 0), "PS": (0, 1),
+        "RP": (0, 1), "RR": (0.5, 0.5), "RS": (1, 0),
+        "SP": (1, 0), "SR": (0, 1), "SS": (0.5, 0.5)
+    }
+
+    i = p1[1] + p2[1]
+    r = results[i]
+
+    return [(p1[0], r[0]), (p2[0], r[1])]
+
+
+scores = {}
+
 while (True):
-    komB, adres = UDPServerSocket.recvfrom(bufSize)
-    print(komB)
-    print(adres)
-    # wysyłanie odpowiedzi
-    kom = komB.decode()
-    odpB = kom.upper().encode()
-    UDPServerSocket.sendto(odpB, adres)
+    move, adres = UDPServerSocket.recvfrom(bufSize)
+    move = move.decode()
+
+    if move == "koniec":
+        print("ending game\n")
+        for k in scores.keys():
+            if k != adres:
+                UDPServerSocket.sendto(struct.pack('!f', -1), k)
+        scores = {}
+        continue
+
+    if adres not in scores:
+        if len(scores) < 2:
+            print("new player!")
+            scores.update({adres: {"score": 0, "move": None}})
+        else:
+            UDPServerSocket.sendto(struct.pack('!f', 403), adres)
+            continue
+
+    scores[adres]["move"] = move
+
+    moves = []
+    for k, v in scores.items():
+        if v["move"] is not None:
+            moves.append((k, v["move"]))
+
+    if len(moves) != 2:
+        continue
+
+    result = rock_paper_scissors(moves)
+
+    print("Scores:")
+    for r in result:
+        scores[r[0]]["score"] += r[1]
+        print(r[0], scores[r[0]])
+        scores[r[0]]["move"] = None
+        UDPServerSocket.sendto(struct.pack('!f', r[1]), r[0])
+
+    # pprint.pprint(scores)
