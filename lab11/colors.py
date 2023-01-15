6110a4e158e9b084aeeb3b3d6e8fad6560eaa842
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-01-15 22:50:53 +0100

    change color

--- lab11/colors.py
+++ lab11/colors.py
@@ -2,10 +2,9 @@ import random
 
 
 def get_random_colors():
-    colors = ["white", "red", "green", "blue", "yellow", "pink",
+    colors = ["white", "red", "green", "blue", "yellow", "deep pink",
               "cyan", "lime green", "saddle brown", "orange", "gray", "purple"]
     colors.extend(colors)
 
     random.shuffle(colors)
-
     return [colors[i:i+6] for i in range(0, 19, 6)]
