afbfdc62fa4ed438008e0aac262185e2baa92da0
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-01-15 18:29:21 +0100

    add threading to gui for sending and reciving moves

--- lab11/board.py
+++ lab11/board.py
@@ -4,13 +4,15 @@ import struct
 import socket
 import time
 import json
+import queue
+import threading
 
 sg.theme('Dark Blue 3')
 
 layout = [[sg.Text('Choose your game port', key='-TITLE-')],
           [sg.Input(default_text="5001", key="-INPUT-")],
           [sg.Text('', key='-ERROR-')],
-          [sg.Button('OK'), sg.Exit()]]
+          [sg.Button('OK', bind_return_key=True), sg.Exit()]]
 
 window = sg.Window('Game setup', layout)
 
@@ -24,7 +26,8 @@ opponent_address_port = None
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
-        break
+        window.close()
+        exit()
     print(event, values)
     window["-ERROR-"].update("")
 
@@ -50,40 +53,113 @@ window.close()
 ############################################################
 UDPSocket.sendto(str.encode("start"), opponent_address_port)
 
+# randomazied list of pair colors made by pplayer that first joins
 colors = None
+# determines if it is player turn to play
+turn = False
 
 while True:
     try:
         move, adres = UDPSocket.recvfrom(bufSize)
         move = move.decode()
-        print(move, adres)
         if move == "start":
             colors = get_random_colors()
             UDPSocket.sendto(str.encode(json.dumps(colors)),
                              opponent_address_port)
+            turn = True
             break
         else:
             colors = json.loads(move)
             break
-
     except:
         time.sleep(1)
 
-print(colors)
+########################################################################
 
-layout = [[sg.B(' ', size=(8, 4), key=(j, i), button_color="black")
-           for i in range(6)] for j in range(4)]
+
+def long_operation_thread(gui_queue):
+    """
+    A worker thread that communicates with the GUI through a queue
+    This thread can block for as long as it wants and the GUI will not be affected
+    :param gui_queue: (queue.Queue) Queue to communicate back to GUI that task is completed
+    :return:
+    """
+    print("Starting thread")
+    move, adres = UDPSocket.recvfrom(bufSize)
+    gui_queue.put(move.decode())  # put a move into queue for GUI
+
+
+# queue used to communicate between the gui and the threads
+gui_queue = queue.Queue()
+
+layout = [[sg.Text('Your turn' if turn else "Opponent turn", key='-TITLE-'), sg.Text("Points: 0", key='-POINTS-')],
+          [[sg.B(' ', size=(8, 4), key=(j, i), button_color="black", disabled=not turn)
+           for i in range(6)] for j in range(4)]]
 
 window = sg.Window('Memory Game', layout)
 
+
+def update_gui():
+    window["-TITLE-"].update('Your turn' if turn else "Opponent turn")
+    for i in range(6):
+        for j in range(4):
+            window[(j, i)].update(disabled=not turn)
+
+
+points = 0
+moves_remaining = 2
+thread_flag = True
+
 # Event Loop
 while True:
-    event, values = window.read()
-    print(event, values)
+    event, values = window.Read(timeout=100)
+    if (event != "__TIMEOUT__"):
+        print(event, values)
+        print(moves_remaining, turn, thread_flag)
     if event in (None, 'Exit'):
         break
-    # current_marker = window[event].get_text()
-    # window[event].update('X' if current_marker ==
-    #                      ' ' else 'O' if current_marker == 'X' else ' ')
-    window[event].update(button_color=colors[event[0]][event[1]])
+
+    # player move
+    if turn and type(event) is tuple:
+        if moves_remaining > 0:
+            window[event].update(button_color=colors[event[0]][event[1]])
+            moves_remaining -= 1
+            UDPSocket.sendto(str.encode(json.dumps({"move": event, "next": False})),
+                             opponent_address_port)
+        if moves_remaining == 0:
+            turn = False
+            update_gui()
+
+            UDPSocket.sendto(str.encode(json.dumps({"move": None, "next": True})),
+                             opponent_address_port)
+    # start threads to get moves form other player
+    if not turn and thread_flag:
+        for _ in range(3):
+            threading.Thread(target=long_operation_thread,
+                             args=(gui_queue,), daemon=True).start()
+        thread_flag = False
+
+    # --------------- Check for incoming moves from threads  ---------------
+    try:
+        move = gui_queue.get_nowait()
+        move = json.loads(move)
+    except queue.Empty:             # get_nowait() will get exception when Queue is empty
+        move = None              # break from the loop if no more moves are queued up
+
+    # if move received from queue, display the move in the Window
+    if move:
+        print('Got a move back from the thread: ')
+        print(move)
+        tile = move["move"]
+        if tile != None:
+            # make list hashable by turning it into a tuple
+            tile = tuple(tile)
+            window[tile].update(button_color=colors[tile[0]][tile[1]])
+
+        if move["next"]:
+            turn = True
+            thread_flag = True
+            moves_remaining = 2
+            update_gui()
+
 window.close()
